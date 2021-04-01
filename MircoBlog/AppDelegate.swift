d9c85acd1f8b3b9d27ac40a2439a328c8e27bc3f
mzx-699 <978653881@qq.com>
2021-04-01 17:14:19 +0800

    创建各个控制器模版文件

--- MircoBlog/AppDelegate.swift
+++ MircoBlog/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = UIColor.white
-        window?.rootViewController = ViewController()
+        window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
         return true
     }
